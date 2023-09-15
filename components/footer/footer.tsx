1ee7223f4099d06f06f022b8b9a9c14c6906b348
juwon <hajiung25@gmail.com>
2023-09-15 18:03:03 +0900

    feat: 페이지 기본 시멘틱 레이아웃 추가

---
+++ components/footer/footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrap = styled.footer`
+  width: 100vw;
+  height: 450px;
+  background-color: #2E2E2E;
+`
+
+function Footer() {
+  return (
+    <Wrap>
+      
+    </Wrap>
+  )
+}
+
+export default Footer
---
+++ components/header/header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrap = styled.header`
+  width: 100vw;
+  height: 70px;
+  background: #000000;
+`
+
+function Header() {
+  return (
+    <Wrap>
+      
+    </Wrap>
+  )
+}
+
+export default Header
